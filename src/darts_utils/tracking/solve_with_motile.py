424e9039112a63b5dd587b91b62d570c7cffa5ab
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-08-12 14:34:36 -0400

    Add area difference cost

--- src/darts_utils/tracking/solve_with_motile.py
+++ src/darts_utils/tracking/solve_with_motile.py
@@ -1,6 +1,5 @@
 import motile
 from motile_toolbox.candidate_graph import graph_to_nx
-from darts_utils.tracking import utils
 
 
 def solve_with_motile(config, graph, exclusion_sets):
@@ -14,41 +13,55 @@ def solve_with_motile(config, graph, exclusion_sets):
     """
     solver = motile.Solver(graph)
 
-    solver.add_cost(motile.costs.EdgeSelection(
-        weight= config["edge_selection_weight"], 
-        attribute="drift_dist", 
-        constant = config["edge_selection_constant"]
-    ))
-
     solver.add_constraint(motile.constraints.MaxParents(1))
-    solver.add_constraint(motile.constraints.MaxChildren(2))
+    solver.add_constraint(motile.constraints.MaxChildren(1))
 
-    # solver.add_cost(utils.HyperAreaSplit(
-    #     weight=config["hyper_weight"],
-    #     constant=config["hyper_constant"],
-    #     area_attribute= "area"
-    # ))
+    solver.add_cost(
+        motile.costs.EdgeSelection(
+            weight=config["drift_weight"],
+            attribute="drift_dist",
+            constant=config["drift_constant"],
+        ),
+        name="drift",
+    )
 
-    solver.add_cost(motile.costs.NodeSelection(
-        weight=config["cohesion_weight"], 
-        attribute="cohesion", 
-        constant=config["cohesion_constant"]
-    ), name="cohesion")
-    
-    solver.add_cost(motile.costs.NodeSelection(
-        weight=config["adhesion_weight"], 
-        attribute="adhesion", 
-        constant=config["adhesion_constant"],
-    ), name="adhesion")
-    
-    solver.add_cost(motile.costs.Appear(
-        constant=config["appear_constant"], 
-        ignore_attribute = "ignore_appear"
-    ))
-    solver.add_cost(motile.costs.Disappear(
-        constant=config["disappear_constant"], 
-        ignore_attribute = "ignore_disappear"
-    ))
+    solver.add_cost(
+        motile.costs.EdgeSelection(
+            weight=config["area_weight"],
+            attribute="area_diff",
+            constant=config["area_constant"],
+        ),
+        name="area",
+    )
+
+    solver.add_cost(
+        motile.costs.NodeSelection(
+            weight=config["cohesion_weight"],
+            attribute="cohesion",
+            constant=config["cohesion_constant"],
+        ),
+        name="cohesion",
+    )
+
+    solver.add_cost(
+        motile.costs.NodeSelection(
+            weight=config["adhesion_weight"],
+            attribute="adhesion",
+            constant=config["adhesion_constant"],
+        ),
+        name="adhesion",
+    )
+
+    solver.add_cost(
+        motile.costs.Appear(
+            constant=config["appear_constant"], ignore_attribute="ignore_appear"
+        )
+    )
+    solver.add_cost(
+        motile.costs.Disappear(
+            constant=config["disappear_constant"], ignore_attribute="ignore_disappear"
+        )
+    )
 
     solver.add_constraint(motile.constraints.ExclusiveNodes(exclusion_sets))
 
