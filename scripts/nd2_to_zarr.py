3f6878cbb9fbd664ee8703abf71eafd50393003c
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-09-04 12:45:53 -0400

    Add metadata correctly to zarr from nd2

--- scripts/nd2_to_zarr.py
+++ scripts/nd2_to_zarr.py
@@ -1,36 +1,34 @@
 from pprint import pprint
+from dataclasses import asdict
+import ome_types
 
 import nd2
 import zarr
 
-infile = "/Volumes/funke/data/darts/240411_phase_fluor/110424_LIB518_40xPh_epi.nd2"
-outfile = "/Volumes/funke/data/darts/240411_phase_fluor/240411_phase_fluor.zarr"
+infile = "/nrs/funke/data/darts/240411_phase_fluor/110424_LIB518_40xPh_epi.nd2"
+outfile = "/nrs/funke/data/darts/240411_phase_fluor/240411_phase_fluor.zarr"
 outgroup = "raw"
 
 with nd2.ND2File(infile) as myfile:
-    metadata = myfile.metadata
-    attributes = myfile.attributes
-    ome_metadata = myfile.ome_metadata()
-    num_fovs = myfile.shape[1]
-    # pprint(myfile.attributes)
-    pprint(myfile.shape)
-    pprint(myfile.ndim)
-    pprint(myfile.dtype)
-    pprint(myfile.sizes)
-    pprint(myfile.voxel_size())
+    # save all metadata in the zarr attrs
+    root = zarr.open(outfile)
+    root.attrs.update({"metadata": asdict(myfile.metadata)})
+    root.attrs.update({"attributes": myfile.attributes._asdict()})
+    root.attrs.update({"ome_metadata": myfile.ome_metadata().model_dump_json()})
+    # include special channels attribute because we know we need it
+    channels = next(iter(myfile.ome_metadata().images)).pixels.channels
+    channels_dict = {"channels": [channel.model_dump_json() for channel in channels]}
+    root.attrs.update(channels_dict)
 
     data = myfile.to_dask().rechunk(chunks=(1, 1, 1, 2960, 5056))
     print(data.chunksize)
 
-    channels = next(iter(ome_metadata.images)).pixels.channels
-    channels_dict = {"channels": [channel.model_dump_json() for channel in channels]}
-    root = zarr.open(outfile)
-    root.attrs.put(channels_dict)
-
+    num_fovs = myfile.shape[1] 
     for fov in range(num_fovs):
+        if fov <= 5:
+            continue
         fov_data = data[:, fov]
-        pprint(fov_data.shape)
         fov_group = f"{fov}/{outgroup}"
         print(fov_group)
+        pprint(fov_data.shape)
         fov_data.to_zarr(url=outfile, component=fov_group, dimension_separator="/")
-        break
